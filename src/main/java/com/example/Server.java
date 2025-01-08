4729c35e241e0443c448c862f6cbc9b57b8cf9e7
Wojciech Opara <wwojtek660@gmail.com>
2025-01-08 20:05:42 +0100

    Add book table to database & handle multithreading

--- src/main/java/com/example/Server.java
+++ src/main/java/com/example/Server.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.net.*;
 import java.io.*;
+import java.sql.SQLException;
 
 public class Server {
     private ServerSocket serverSocket;
@@ -10,22 +11,30 @@ public class Server {
     private BufferedReader in;
     private DbConnection dbConnection;
 
-    public void start(int port) throws IOException {
+    public void start(int port) throws IOException, SQLException {
         serverSocket = new ServerSocket(port);
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         dbConnection = new DbConnection();
 
-        String message = in.readLine();
-        out.println(message);
+        String message;
+
+        while ((message = in.readLine()) != null) {
+            // echo the message back to the client
+            out.println(message);
+        }
     }
 
-    public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-        dbConnection.close();
+    public void stop() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+            dbConnection.close();
+        } catch (IOException | SQLException e) {
+            System.out.println(e.toString());
+        }
     }
 }
