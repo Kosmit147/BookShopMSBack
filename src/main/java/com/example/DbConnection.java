e7b7fd3130886e28df6141a581bb2994c1eb56dd
Wojciech Opara <wwojtek660@gmail.com>
2025-01-08 20:05:42 +0100

    Add book table to database & handle multithreading

--- src/main/java/com/example/DbConnection.java
+++ src/main/java/com/example/DbConnection.java
@@ -4,51 +4,50 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.ResultSet;
 
 public class DbConnection {
     public static final String url = "jdbc:sqlite:bookshop.db";
 
     Connection connection;
 
-    DbConnection() {
-        try {
-            connection = DriverManager.getConnection(url);
-            System.out.println("Connected to database.");
+    DbConnection() throws SQLException {
+        connection = DriverManager.getConnection(url);
+        System.out.println("Connected to database.");
 
-            Statement stmt = connection.createStatement();
+        createTables();
 
-            String createTableQuery = """
+        // Statement stmt = connection.createStatement();
+        // String insertQuery = "INSERT INTO books (title) VALUES ('1984')";
+        // stmt.executeUpdate(insertQuery);
+
+        // String selectQuery = "SELECT * FROM books";
+        // ResultSet resultSet = stmt.executeQuery(selectQuery);
+
+        // System.out.println("Query Results:");
+
+        // while (resultSet.next()) {
+        //     int id = resultSet.getInt("id");
+        //     String title = resultSet.getString("title");
+        //     System.out.printf("ID: %d, Title: %s%n", id, title);
+        // }
+    }
+
+    public void close() throws SQLException {
+        connection.close();
+    }
+
+    private void createTables() throws SQLException {
+        Statement stmt = connection.createStatement();
+
+        String createTableQuery = """
                 CREATE TABLE IF NOT EXISTS books (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    title TEXT NOT NULL
+                    title TEXT NOT NULL,
+                    author TEXT NOT NULL,
+                    price REAL NOT NULL
                 );
                 """;
 
-            stmt.execute(createTableQuery);
-            String insertQuery = "INSERT INTO books (title) VALUES ('1984')";
-            stmt.executeUpdate(insertQuery);
-
-            String selectQuery = "SELECT * FROM books";
-            ResultSet resultSet = stmt.executeQuery(selectQuery);
-
-            System.out.println("Query Results:");
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String title = resultSet.getString("title");
-                System.out.printf("ID: %d, Title: %s%n", id, title);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-        }
-    }
-
-    public void close() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-        }
+        stmt.execute(createTableQuery);
     }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,42 @@
 package com.example;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
+    private static AtomicBoolean running = new AtomicBoolean(true);
     public static final int port = 666;
 
     public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down server...");
+            running.set(false);
+        }));
+
         try {
-            Server server = new Server();
-            server.start(port);
-        } catch (IOException e) {
+            Thread serverThread = new Thread(Main::serverThread);
+            serverThread.start();
+
+            while (running.get()) {
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
             System.out.println(e.toString());
+        }
+    }
+
+    private static void serverThread()
+    {
+        Server server = new Server();
+
+        try {
+            server.start(port);
+        } catch (IOException | SQLException e) {
+            System.out.println(e.toString());
+        } finally {
+            server.stop();
+            running.set(false);
         }
     }
 }
